e3a841951d8e9fd5bbfe1bc45139b42d7c9a7ff8
Szücs Gábor Attila <gabor.attila.szucs@gmail.com>
2024-04-10 22:59:07 +0200

    Create

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+int ExitCode = 0;
+ProcessStartInfo ProcessInfo;
+Process Process;
+
+if (string.IsNullOrEmpty(args[0]))
+{
+    Environment.Exit(8888);
+}
+
+ProcessInfo = new ProcessStartInfo("cmd.exe", "/c " + args[0]);
+ProcessInfo.CreateNoWindow = true;
+ProcessInfo.UseShellExecute = false;
+
+Process = Process.Start(ProcessInfo);
+Process.WaitForExit();
+
+ExitCode = Process.ExitCode;
+Process.Close();
+if (ExitCode == 0)
+{
+    Environment.Exit(0);
+}
+else
+{
+    Environment.Exit(ExitCode);
+}
